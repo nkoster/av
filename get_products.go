2e9c52a14bfc3cfd9c0e25fc001a7148127ea70d
Niels <n.koster@portavita.eu>
2024-02-20 23:44:29 +0100

    Retrieve and incorporate all product data into JSON format

--- get_products.go
+++ get_products.go
@@ -7,44 +7,53 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Product struct {
+	ID       int     `json:"id"`
+	Title    string  `json:"title"`
+	UrlTitle string  `json:"url_title"`
+	Image    string  `json:"image"`
+	Descr    string  `json:"descr"`
+	Specs    string  `json:"specs"`
+	Price    int     `json:"price"`
+	Weight   int     `json:"weight"`
+}
+
 func getProducts(c *fiber.Ctx) error {
-
 	defer timeTrack(time.Now(), "getProducts")
-
-	titles, err := getProductTitles()
+	products, err := getProductsDetails()
 	if err != nil {
 		fmt.Println(err)
-		// Terugsturen van een serverfout als het ophalen van de producttitels mislukt
+		// Terugsturen van een serverfout als het ophalen van de productgegevens mislukt
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Kan producttitels niet ophalen",
+			"error": "Kan productgegevens niet ophalen",
 		})
 	}
 
 	return c.JSON(fiber.Map{
-		"titles": titles,
+		"products": products,
 	})
 }
 
-func getProductTitles() ([]string, error) {
+func getProductsDetails() ([]Product, error) {
+	var products []Product
 
-	var titles []string
-	rows, err := db.Query("SELECT title FROM products")
+	rows, err := db.Query("SELECT id, title, url_title, image, descr, specs, price, weight FROM products")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var title string
-		if err := rows.Scan(&title); err != nil {
+		var p Product
+		if err := rows.Scan(&p.ID, &p.Title, &p.UrlTitle, &p.Image, &p.Descr, &p.Specs, &p.Price, &p.Weight); err != nil {
 			return nil, err
 		}
-		titles = append(titles, title)
+		products = append(products, p)
 	}
 
 	if err = rows.Err(); err != nil {
 		return nil, err
 	}
 
-	return titles, nil
+	return products, nil
 }
