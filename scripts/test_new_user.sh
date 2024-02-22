230fc7a92214f98a0cb3a5f660d0665a2cda432f
Niels <n.koster@portavita.eu>
2024-02-22 01:48:14 +0100

    Initial, test script for adding a new user

---
+++ scripts/test_new_user.sh
@@ -0,0 +1,6 @@
+curl -X POST http://localhost:3000/newuser \
+     -H "Content-Type: application/json" \
+     -d '{
+            "username":"nieuweGebruiker",
+            "password":"sterkWachtwoord"
+        }'
