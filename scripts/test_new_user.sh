5b300df32f203ac5ecc5fe2577fb8a1a8dc2dd3e
Niels <n.koster@portavita.eu>
2024-02-25 21:05:43 +0100

    Implement the extended User object

--- scripts/test_new_user.sh
+++ scripts/test_new_user.sh
@@ -1,6 +1,14 @@
 curl -X POST http://localhost:3000/newuser \
      -H "Content-Type: application/json" \
      -d '{
-            "username":"nieuweGebruiker",
-            "password":"sterkWachtwoord"
+          "first_name":"Niels",
+          "last_name":"Koster",
+          "username":"niels",
+          "password":"apekop",
+          "email":"niels@w3b.net",
+          "phone":"0651123841",
+          "street_name":"peppengouw",
+          "house_number":"7",
+          "city":"Almere",
+          "postal_code":"1351 NA"
         }'
