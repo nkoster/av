549a8fe82a00033cc248bafdf9452d7149972f83
Niels <n.koster@portavita.eu>
2024-02-26 21:57:59 +0100

    Introduce modulo-like functionality in GoFiber template engine

--- types.go
+++ types.go
@@ -12,18 +12,19 @@ type Product struct {
 	Length   int     `json:"length"`
 	Width    int     `json:"width"`
 	Height   int     `json:"height"`
+	IsThird  bool // tbv ontbrekende mod functie in de html template engine
 }
 
-type User struct {
-	ID string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Email string `json:"email"`
-	Phone string `json:"phone"`
-	StreetName string `json:"street_name"`
+  type User struct {
+	ID          string `json:"id"`
+	FirstName   string `json:"first_name"`
+	LastName    string `json:"last_name"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	Phone       string `json:"phone"`
+	StreetName  string `json:"street_name"`
 	HouseNumber string `json:"house_number"`
-	City string `json:"city"`
-	PostalCode string `json:"postal_code"`
+	City        string `json:"city"`
+	PostalCode  string `json:"postal_code"`
 }
