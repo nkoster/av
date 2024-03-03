c0319ba42f8c45c1e867b80005b7d90f9445e954
Niels <n.koster@portavita.eu>
2024-03-03 23:33:06 +0100

    Initial, function for processing image uploads

---
+++ upload_images.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func uploadImages(c *fiber.Ctx) error {
+	// Map waar de geüploade afbeeldingen worden opgeslagen
+	uploadDir := "./ui/images/p"
+
+	// Parse de multipart form
+	form, err := c.MultipartForm()
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Kan upload niet verwerken"})
+	}
+
+	files := form.File["imagesUpload[]"]
+	var uploadedFiles []string
+
+	for _, file := range files {
+		// Gebruik de originele bestandsnaam
+		filePath := filepath.Join(uploadDir, filepath.Base(file.Filename))
+
+		// Sla het bestand op in de aangegeven map
+		if err := c.SaveFile(file, filePath); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Kan bestand niet opslaan"})
+		}
+
+		// Voeg de bestandsnaam toe aan de lijst van geüploade bestanden
+		uploadedFiles = append(uploadedFiles, file.Filename)
+	}
+
+	// Retourneer de lijst van bestandsnamen gescheiden door komma's
+	return c.JSON(fiber.Map{"images": uploadedFiles})
+}
+
+
