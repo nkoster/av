217835f8985cbe73add7761d7e642f8ed5872291
Niels Koster <niels.koster@portavita.nl>
2024-06-08 14:49:57 +0200

    Do not reply with thumbnail filenames in list.

--- upload_images.go
+++ upload_images.go
@@ -7,39 +7,27 @@ import (
 )
 
 func uploadImages(c *fiber.Ctx) error {
-	// Map waar de geüploade afbeeldingen worden opgeslagen
 	uploadDir := "./ui/images/p"
 
-	// Parse de multipart form
 	form, err := c.MultipartForm()
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Kan upload niet verwerken"})
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot process upload"})
 	}
 
 	files := form.File["imagesUpload[]"]
 	var uploadedFiles []string
 
 	for _, file := range files {
-		// Gebruik de originele bestandsnaam
+		// Use original filename
 		filePath := filepath.Join(uploadDir, filepath.Base(file.Filename))
 
-		// Sla het bestand op in de aangegeven map
 		if err := c.SaveFile(file, filePath); err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Kan bestand niet opslaan"})
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot save file"})
 		}
 
-		// Voeg de bestandsnaam toe aan de lijst van geüploade bestanden
 		uploadedFiles = append(uploadedFiles, file.Filename)
-
-		// Maak een thumbnail naam en voeg die ook toe aan de lijst
-		thumbnailName := "thumbnail_" + file.Filename
-		// (genereer thumbnails hier)
-		uploadedFiles = append(uploadedFiles, thumbnailName)
 
 	}
 
-	// Retourneer de lijst van bestandsnamen gescheiden door komma's
 	return c.JSON(fiber.Map{"images": uploadedFiles})
 }
-
-
