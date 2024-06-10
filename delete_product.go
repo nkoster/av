93c1d1bdd6674a38efdb7cbfef26ff2c15fc07b6
Niels Koster <n.koster@portavita.eu>
2024-06-10 21:56:01 +0200

    Reload page after product removal + Cleanup

--- delete_product.go
+++ delete_product.go
@@ -9,33 +9,25 @@ import (
 
 func deleteProduct(c *fiber.Ctx) error {
 
-	// ID uit URL parameter halen
+	// Get ID from URL parameter
 	idParam := c.Params("id")
 	id, err := strconv.Atoi(idParam)
 	if err != nil {
-		// Als ID geen integer is, geef een foutmelding terug
+		// Error when product ID != integer
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Ongeldig product ID",
+			"error": "Invalid product ID",
 		})
 	}
-
-	// if err := c.BodyParser(&product); err != nil {
-	// 	fmt.Println(err)
-	// 	return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-	// 		"error": "Kan productgegevens niet verwerken",
-	// 	})
-	// }
 
 	_, err = db.Exec("DELETE FROM products WHERE id = $1", id)
 	if err != nil {
 		fmt.Println(err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Kan product niet verwijderen uit database",
+			"error": "Removing product failed!",
 		})
 	}
 
-	fmt.Println("id", id, "verwjderd")
-	return c.JSON(fiber.Map{
-		"message": "Product succesvol verwijderd",
-	})
+	fmt.Println("id", id, "removed")
+	return c.SendString("<script>window.location.reload();</script>")
+
 }
